6f22b95939d7e24a4d206273936e7806a45ce816
Sghen <945654877@qq.com>
2020-03-28 23:53:34 +0800

    [*]测试滚动事件禁止默认行为

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,13 @@ export default {
 
 <style lang="scss" scoped>
 #app {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
