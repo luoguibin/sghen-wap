f16075db6082da5ed219de58907497626aa76f2e
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2020-04-01 10:23:40 +0800

    [*]登陆

--- src/store/auth.js
+++ src/store/auth.js
@@ -16,28 +16,27 @@ export default {
   actions: {
     login (context, data) {
       return new Promise(function (resolve, reject) {
-        const func = function (data_) {
-          apiPostData(apiURL.login, data_).then(resp => {
+        const func = function (_data, _resolve, _reject) {
+          apiPostData(apiURL.login, _data).then(resp => {
             context.commit('setUserInfo', resp.data)
-            resolve(resp)
+            _resolve(resp)
           }).catch(err => {
-            reject(err)
+            _reject(err)
           })
         }
         if (data.pw) {
           // todo: crypto-js的core中加载了所有crypto包。。。
           require(['crypto-js/enc-base64.js', 'crypto-js/md5.js'], function (Base64, MD5) {
             data.pw = Base64.stringify(MD5(data.pw))
+            func(data, resolve, reject)
           }, function (err) {
             if (err) {
               reject(new Error('配置文件加载失败'))
               window._sgGlobal.$toast('配置文件加载失败')
-            } else {
-              func(data)
             }
           })
         } else {
-          func(data)
+          func(data, resolve, reject)
         }
       })
     },
