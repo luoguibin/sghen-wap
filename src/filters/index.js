2c850e0fd1bb15201619c48e2a86717ad1b21d79
Sghen <945654877@qq.com>
2020-05-29 22:44:00 +0800

    [*]优化时间格式

--- src/filters/index.js
+++ src/filters/index.js
@@ -4,6 +4,20 @@ import { defaultImgSrc } from '@/common/const'
 export const timeFormat = function (date, fmt = 'yyyy-MM-dd hh:mm:ss') {
   // 2018-04-15T10:10:10+08:00
   date = new Date(date)
+  const now = new Date()
+  const ms = now.getTime() - date.getTime()
+  if (ms < 10 * 60 * 1000) {
+    return '几分钟前'
+  } else if (ms < 60 * 60 * 1000) {
+    return Math.floor(ms / 60 / 1000) + '分钟前'
+  }
+  let preffix = ''
+  if (now.getFullYear() === date.getFullYear() &&
+    now.getMonth() === date.getMonth() &&
+    now.getDate() === date.getDate()) {
+    fmt = fmt.split(' ')[1]
+    preffix = '今天'
+  }
   const o = {
     'M+': date.getMonth() + 1, // 月份
     'd+': date.getDate(), // 日
@@ -29,7 +43,7 @@ export const timeFormat = function (date, fmt = 'yyyy-MM-dd hh:mm:ss') {
       )
     }
   }
-  return fmt
+  return preffix + fmt
 }
 
 export const imgSrcFormat = function (v) {
