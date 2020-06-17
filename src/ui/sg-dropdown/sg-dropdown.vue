2031db117542b6c84220c654ef820aa6a4c0001a
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2020-06-17 10:43:22 +0800

    [*]优化sg-dropdown

--- src/ui/sg-dropdown/sg-dropdown.vue
+++ src/ui/sg-dropdown/sg-dropdown.vue
@@ -1,29 +1,37 @@
 <template>
-  <div
-    :class="['sg-dropdown', optionsVisible ? 'sg-dropdown-active' : '', 'sg-dropdown_' + direction]"
-  >
-    <div class="sg-dropdown-label sg-flex" @click.stop="onToggle">
+  <div :class="['sg-dropdown', optionsVisible ? 'options-active' : '']">
+    <div class="option-label sg-flex" @click.stop="onToggle">
       <div class="sg-flex-one sg-ellipsis">
         <slot>
           <span
-            :class="{'sg-dropdown-tip': !currentLabel, 'sg-dropdown-text': optionActive && !!currentLabel}"
+            :class="{'option-tip': !currentLabel, 'current-label': optionActive && !!currentLabel}"
           >{{currentLabel || '点击选择'}}</span>
         </slot>
       </div>
       <i v-show="pointerVisible" class="iconfont icon-down"></i>
     </div>
-    <div v-show="optionsVisible" class="sg-dropdown-options">
-      <div class="options-wrapper">
-        <div
-          v-for="item in options"
-          :key="item.value"
-          class="sg-dropdown-option sg-ellipsis"
-          :class="{'sg-dropdown-option_active': optionActive && currentValue === item.value}"
-          @click.stop="onSelectOption(item, $event)"
-        >{{item.label}}</div>
+    <div
+      v-show="optionsVisible"
+      :class="[optionType !== 'center' ? '' : 'sg-mask', 'dropdown-' + optionType, 'align-' + optionAlign]"
+    >
+      <div class="sg-options">
+        <div class="options-inner">
+          <div
+            v-for="item in options"
+            :key="item.value"
+            v-show="!item.hidden"
+            class="sg-option sg-ellipsis"
+            :class="{'option-active': optionActive && currentValue === item.value}"
+            :disabled="item.disabled"
+            @click.stop="onSelectOption(item, $event)"
+          >
+            <i v-if="item.icon" :class="['iconfont', item.icon]"></i>
+            {{item.label}}
+          </div>
+        </div>
       </div>
+      <div v-if="optionType !== 'center'" v-show="optionsVisible" class="pointer"></div>
     </div>
-    <div v-show="optionsVisible" class="sg-dropdown-pointer"></div>
   </div>
 </template>
 
@@ -38,17 +46,25 @@ export default {
     },
     optionActive: {
       type: Boolean,
-      default: false
+      default: true
+    },
+    optionType: {
+      type: String,
+      default: 'normal' // ['normal', 'fullwidth', 'center']
     },
     pointerVisible: {
       type: Boolean,
       default: true
+    },
+    initValue: {
+      type: String,
+      default: ''
     }
   },
 
   data () {
     return {
-      direction: 'bottom',
+      optionAlign: 'bottom',
       optionsVisible: false,
       currentLabel: '',
       currentValue: ''
@@ -61,16 +77,22 @@ export default {
         this.optionsVisible = false
       }
     })
+
+    const option = this.options.find(o => o.value === this.initValue)
+    option && this.setSelectOption(option)
   },
 
   methods: {
     onToggle () {
       this.optionsVisible = !this.optionsVisible
       if (this.optionsVisible) {
-        const rect = this.$el.getBoundingClientRect()
-        const isElTop = rect.top < document.body.clientHeight / 2
-        // console.log('isElTop', isElTop)
-        this.direction = isElTop ? 'bottom' : 'top'
+        if (this.optionType === 'center') {
+          this.optionAlign = 'bottom'
+        } else {
+          const rect = this.$el.getBoundingClientRect()
+          const isElBottom = rect.top > document.body.clientHeight / 2
+          this.optionAlign = isElBottom ? 'top' : 'bottom'
+        }
       }
     },
     setSelectOption ({ value, label }) {
@@ -78,6 +100,9 @@ export default {
       this.currentLabel = label
     },
     onSelectOption (option, e) {
+      if (option.disabled) {
+        return
+      }
       this.setSelectOption(option)
       this.optionsVisible = false
       this.$emit('change', option.value, e)
@@ -88,25 +113,28 @@ export default {
 
 <style lang="scss" scoped>
 @import "../style/const.scss";
+// @import "../style/index.scss";
 
 .sg-dropdown {
   position: relative;
-  padding: $padding-small;
-  height: $height-text;
-  line-height: $height-text;
+  padding: 0 $padding-small;
   background-color: white;
+  box-sizing: border-box;
+  // test style
+  // border: 1px solid gray;
+  // background-color: #ddd;
   .sg-flex {
     align-items: center;
   }
-  .sg-dropdown-tip {
+  .option-tip {
     color: #aaa;
   }
-  .sg-dropdown-text {
+  .current-label {
     color: $color-theme;
   }
-  .sg-dropdown-label {
+  .option-label {
     font-size: 1.4rem;
-    // line-height: 1;
+    line-height: $height-text;
     i {
       display: inline-block;
       margin-left: 1rem;
@@ -115,24 +143,47 @@ export default {
     }
   }
 
-  .sg-dropdown-options {
+  .sg-options {
     position: absolute;
     right: 0;
-    max-width: 50vw;
-    min-width: 5rem;
+    min-width: 8rem;
     padding: $padding-normal 0;
+    // height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
     box-sizing: border-box;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     background-color: white;
     border-radius: 0.3rem;
     z-index: $z-index-normal;
-    .options-wrapper {
-      padding: 0 1rem;
-      max-height: 16rem;
-      overflow: auto;
-    }
+
+    // test style
+    // border: 1px solid gray;
+    // background-color: #ddd;
   }
-  .sg-dropdown-pointer {
+  .options-inner {
+    padding: 0 1rem;
+  }
+
+  .dropdown-normal .sg-options{
+    max-width: 50%;
+    max-height: 20rem;
+  }
+  .dropdown-fullwidth .sg-options {
+    width: 100%;
+    max-height: 20rem;
+  }
+  .dropdown-center .sg-options {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    max-width: 90%;
+    min-width: 5rem;
+    max-height: 80%;
+    transform: translate(-50%, -50%);
+  }
+
+  .pointer {
     position: absolute;
     right: 1rem;
     width: 0;
@@ -142,45 +193,68 @@ export default {
     border-width: 1rem;
     border-style: solid;
     border-color: transparent transparent white transparent;
+
+    // test style
+    // border-color: transparent transparent #ddd transparent;
   }
-  .sg-dropdown-option {
+  .sg-option {
     margin-bottom: 0.5rem;
     line-height: $height-text;
     font-size: $size-text;
     color: $color-text;
+    &[disabled] {
+      color: $color-theme-disabled;
+    }
     &:last-child {
       margin-bottom: 0;
     }
   }
-  .sg-dropdown-option_active {
+  .option-active {
     color: $color-theme;
   }
 }
 
-.sg-dropdown_top {
-  .sg-dropdown-options {
-    bottom: 3rem;
+.dropdown-normal.align-top,
+.dropdown-fullwidth.align-top {
+  .sg-options {
+    bottom: 3.4rem;
   }
-  .sg-dropdown-pointer {
-    bottom: 1rem;
+  .pointer {
+    bottom: 1.4rem;
     transform: rotate(180deg);
   }
 }
 
-.sg-dropdown_bottom {
-  .sg-dropdown-options {
-    top: 3rem;
+.dropdown-normal.align-bottom,
+.dropdown-fullwidth.align-bottom {
+  .sg-options {
+    top: 3.4rem;
   }
-  .sg-dropdown-pointer {
-    top: 1rem;
+  .pointer {
+    top: 1.4rem;
   }
 }
 
-.sg-dropdown-active {
-  .sg-dropdown-label {
+.options-active {
+  .option-label {
     i {
       transform: rotate(180deg);
     }
   }
 }
+</style>
+
+<style>
+/* @import "//at.alicdn.com/t/font_1730652_96vm5rfm5q.css";
+* {
+  margin: 0;
+  padding: 0;
+}
+html {
+  font-size: 12px;
+}
+html,
+body {
+  height: 100%;
+} */
 </style>
