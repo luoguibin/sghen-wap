ffc5391fa1c4c9673c43fed987d92693f5372510
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2020-06-19 17:07:18 +0800

    [!]重构sg-slider

--- src/ui/sg-slider/sg-slider.vue
+++ src/ui/sg-slider/sg-slider.vue
@@ -1,28 +1,24 @@
 <template>
-  <div class="sg-slider">
+  <div
+    :class="{'sg-slider': true, 'slide-anime': !isTouching}"
+    @touchstart="onTouchStart"
+    @touchmove="onTouchMove"
+    @touchend="onTouchEnd"
+    @mousedown="onTouchStart"
+    @mousemove="onTouchMove"
+    @mouseup="onTouchEnd"
+    @click="$emit('click', $event)"
+  >
     <div
-      ref="wrapper"
-      class="wrapper"
-      :style="wrapperStyle"
-      @touchstart="onTouchStart"
-      @touchmove="onTouchMove"
-      @touchend="onTouchEnd"
-      @click="$emit('click', $event)"
+      v-for="item in options"
+      ref="items"
+      :key="item.id"
+      :class="{'slider-item': true, 'card-item__active': item.id === activeId}"
+      :style="item.itemStyle"
+      :item-type="itemType"
     >
-      <div
-        v-for="item in options"
-        :key="item.id"
-        class="card-item-wrapper"
-        :style="item.style"
-        :item-type="itemType"
-      >
-        <div
-          :class="{'card-item': true, 'card-item__active': item.id === activeId}"
-          :style="item.itemStyle"
-        >
-          <slot :name="item.slot"></slot>
-        </div>
-      </div>
+      <!-- <slot :name="item.slot"></slot> -->
+      <div :style="item.contentStyle">{{item}}</div>
     </div>
   </div>
 </template>
@@ -41,30 +37,50 @@ export default {
             slot: 'slot-0',
             itemStyle: {
               backgroundColor: 'rgba(12, 123, 234, 0.3)'
+            },
+            contentStyle: {
+              width: '180%',
+              height: '150%'
             }
           },
           {
             slot: 'slot-1',
             itemStyle: {
               backgroundColor: 'rgba(123, 12, 234, 0.3)'
+            },
+            contentStyle: {
+              width: '150%',
+              height: '100%'
             }
           },
           {
             slot: 'slot-2',
             itemStyle: {
               backgroundColor: 'rgba(234, 12, 123, 0.3)'
+            },
+            contentStyle: {
+              width: '300%',
+              height: '100%'
             }
           },
           {
             slot: 'slot-3',
             itemStyle: {
               backgroundColor: 'rgba(12, 234, 123, 0.3)'
+            },
+            contentStyle: {
+              width: '100%',
+              height: '100%'
             }
           },
           {
             slot: 'slot-4',
             itemStyle: {
               backgroundColor: 'rgba(123, 234, 12, 0.3)'
+            },
+            contentStyle: {
+              width: '100%',
+              height: '100%'
             }
           }
         ]
@@ -79,162 +95,169 @@ export default {
   data () {
     return {
       options: [],
+      isTouching: false,
 
-      transX: 0,
       activeId: 0,
-      activeIndex: 0,
-      swipeDistance: 100
+      activeIndex: 0
     }
   },
 
-  computed: {
-    wrapperStyle () {
-      return {
-        width: `${this.options.length * 100}%`,
-        transform: `translate(${this.transX}px, 0)`
-      }
-    }
-  },
+  computed: {},
 
   created () {
-    window.swipeCards = this
+    window.sgSlider = this
 
     this.options = this.items.map(o => {
       return {
         id: o.slot,
         slot: o.slot,
+        ratioX: 100,
+        contentStyle: o.contentStyle,
         itemStyle: {
           ...o.itemStyle,
-          filter: 'blur(0px)',
-          transform: 'scale(1)'
-        },
-        style: {
-          ...o.style,
-          width: 0,
-          opacity: 1
+          // left: '100%',
+          transform: 'translate(100%, 0)'
         }
       }
     })
-
-    const itemCssWidth = (100 / this.options.length).toFixed(3) + '%'
-    this.options.forEach(o => {
-      o.style.width = itemCssWidth
-    })
+    this.options[0].ratioX = 0
+    // this.options[0].itemStyle.left = 0
+    this.options[0].itemStyle.transform = 'translate(0, 0)'
     this.activeId = this.options[0].id
-  },
-
-  mounted () {
-    // this.swipeDistance = this.$el.clientWidth / 2
+    this.activeIndex = 0
   },
 
   methods: {
     onTouchStart (e) {
-      if (this.isDown) {
-        return
-      }
-      this.isStart = true
-      const touch = e.touches[0]
+      e.preventDefault()
+      this.isTouching = true
+      this.canSlide = true
+      const touch = (e.touches && e.touches[0]) || e
       this.preClientX = touch.clientX
+      this.downClientX = touch.clientX
+      this.downClientY = touch.clientY
+      this.downScrollLeft = this.$refs.items[this.activeIndex].scrollLeft
+      this.downScrollTop = this.$refs.items[this.activeIndex].scrollTop
     },
     onTouchMove (e) {
-      const touch = e.touches[0]
-      this.addTrans(touch.clientX - this.preClientX)
+      if (!this.isTouching) {
+        return
+      }
+      e.preventDefault()
+
+      const touch = (e.touches && e.touches[0]) || e
+      const xVal = touch.clientX - this.preClientX
+      // xVal正数，则当前item向右移动
+      const itemEl = this.$refs.items[this.activeIndex]
+      itemEl.scrollTop = this.downScrollTop - (touch.clientY - this.downClientY)
+      if (itemEl.scrollLeft > 0 && xVal > 0) {
+        this.canSlide = false
+        itemEl.scrollLeft = this.downScrollLeft - (touch.clientX - this.downClientX)
+        this.preClientX = touch.clientX
+        return
+      }
+      if (itemEl.clientWidth + itemEl.scrollLeft < itemEl.scrollWidth && xVal < 0) {
+        this.canSlide = false
+        itemEl.scrollLeft = this.downScrollLeft - (touch.clientX - this.downClientX)
+        this.preClientX = touch.clientX
+        return
+      }
+
+      const index = this.activeIndex
+      const activeItem = this.options[index]
+      const ratio = (xVal * 100) / this.$el.clientWidth
+      activeItem.ratioX += ratio
+      activeItem.ratioX = Math.max(-100, activeItem.ratioX)
+      activeItem.ratioX = Math.min(100, activeItem.ratioX)
+      activeItem.itemStyle.transform = `translate(${activeItem.ratioX}%)`
+      // activeItem.itemStyle.left = `${activeItem.ratioX}%`
+
+      const nextItem = this.options[index + 1]
+      if (nextItem) {
+        // -100% ~ 0 ~ 100%
+        if (activeItem.ratioX <= 0) {
+          nextItem.ratioX = activeItem.ratioX + 100
+        } else {
+          nextItem.ratioX = 100
+        }
+        nextItem.itemStyle.transform = `translate(${nextItem.ratioX}%)`
+        // nextItem.itemStyle.left = `${nextItem.ratioX}%`
+      }
+
+      const previousItem = this.options[index - 1]
+      if (previousItem) {
+        if (activeItem.ratioX >= 0) {
+          previousItem.ratioX = activeItem.ratioX - 100
+        } else {
+          previousItem.ratioX = -100
+        }
+        previousItem.itemStyle.transform = `translate(${previousItem.ratioX}%)`
+        // previousItem.itemStyle.left = `${previousItem.ratioX}%`
+      }
+
       this.preClientX = touch.clientX
     },
-    onTouchEnd () {
-      this.isStart = false
-      this.activeIndex = this.getNextIndex()
-      this.$emit('change', this.activeIndex)
-      this.animeToIndex(this.activeIndex)
-    },
-    setTrans (v) {
-      this.transX = v
-      this.addTrans(0)
-    },
-    addTrans (v) {
-      this.transX += v
-      if (this.transX > this.swipeDistance) {
-        this.transX = this.swipeDistance
+    onTouchEnd (e) {
+      e.preventDefault()
+      this.isTouching = false
+
+      const index = this.activeIndex
+      const activeItem = this.options[index]
+      // <-20% 或者 >20% 则认为进行切换
+      if (activeItem.ratioX < -20) {
+        // 下一页活动
+        this.slideChange(1)
+      } else if (activeItem.ratioX > 20) {
+        // 上一页活动
+        this.slideChange(-1)
       } else {
-        const maxTransX =
-          this.$el.clientWidth * (this.options.length - 1) + this.swipeDistance
-        if (this.transX < -maxTransX) {
-          this.transX = -maxTransX
-        }
+        // 当前页活动
+        this.slideChange()
       }
-      this.calItem()
     },
-    getNextIndex () {
-      let nextIndexValue = 0
-      // 当前滑动得间距
-      const value = this.transX - this.getItemTrans(this.activeIndex)
-      const count = 0 // Math.floor(Math.abs(this.transX) / this.$el.clientWidth)
-      if (value > this.swipeDistance) {
-        nextIndexValue = -(1 + count)
-      } else if (value < -this.swipeDistance) {
-        nextIndexValue = 1 + count
-      }
+    slideChange (unit = 0) {
+      const index = this.activeIndex
+      const activeItem = this.options[index]
+      const previousItem = this.options[index - 1]
+      const nextItem = this.options[index + 1]
 
-      let nextIndex = this.activeIndex + nextIndexValue
-      if (nextIndex < 0) {
-        nextIndex = 0
-      } else if (nextIndex >= this.options.length) {
-        nextIndex = this.options.length - 1
-      }
-      return nextIndex
-    },
-    getItemTrans (index) {
-      return -this.$el.clientWidth * index
-    },
-    calItem () {
-      // 0: -101 + -100%*-1 ~  -100 + -100%*0
-      // 1: -101 + -100%*0  ~  -100 + -100%*1
-      // 2: -101 + -100%*1  ~  -100 + -100%*2
-      // 2: -101 + -100%*2  ~  -100 + -100%*3
-      const clientWidth = this.$el.clientWidth
-      const transX = this.transX
-      const firstVisibleIndex = Math.max(Math.floor(-transX / clientWidth), 0)
-      const ratio = Math.abs(-transX / clientWidth) % 1
-
-      // console.log(firstVisibleIndex, ratio)
-      this.setItemRatio(firstVisibleIndex, ratio)
-    },
-    animeToIndex (index) {
-      const v = this.getItemTrans(index)
-      const stepValue = v - this.transX > 0 ? 30 : -30
-      const stepCount = Math.floor(
-        Math.abs(v - this.transX) / Math.abs(stepValue)
-      )
-
-      cancelAnimationFrame(this.frameTimer)
-      let stepIndex = 0
-      const loop = () => {
-        this.addTrans(stepValue)
-        stepIndex++
-        // console.log(this.transX, stepValue, stepIndex, stepCount)
-        if (stepIndex > stepCount) {
-          this.setTrans(v)
+      if (unit > 0) {
+        if (nextItem) {
+          activeItem.ratioX = -100
+          this.activeIndex++
+          nextItem.ratioX = 0
+          nextItem.itemStyle.transform = `translate(${nextItem.ratioX}%)`
+          // nextItem.itemStyle.left = `${nextItem.ratioX}%`
         } else {
-          this.frameTimer = requestAnimationFrame(loop)
-          // setTimeout(loop, 100)
+          activeItem.ratioX = 0
+        }
+      } else if (unit < 0) {
+        if (previousItem) {
+          activeItem.ratioX = 100
+          this.activeIndex--
+          previousItem.ratioX = 0
+          previousItem.itemStyle.transform = `translate(${previousItem.ratioX}%)`
+          // previousItem.itemStyle.left = `${previousItem.ratioX}%`
+        } else {
+          activeItem.ratioX = 0
+        }
+      } else {
+        activeItem.ratioX = 0
+        if (previousItem) {
+          previousItem.ratioX = -100
+          previousItem.itemStyle.transform = `translate(${previousItem.ratioX}%)`
+          // previousItem.itemStyle.left = `${previousItem.ratioX}%`
+        }
+        if (nextItem) {
+          nextItem.ratioX = 100
+          nextItem.itemStyle.transform = `translate(${nextItem.ratioX}%)`
+          // nextItem.itemStyle.left = `${nextItem.ratioX}%`
         }
       }
-      loop()
-    },
-    setItemRatio (index, ratio) {
-      let itemStyle = this.options[index].itemStyle
-      itemStyle.transform = `scale(${Math.max(1 - ratio, 0.8)})`
-      itemStyle.filter = `blur(${ratio * 30}px)`
-      if (index !== 0) {
-        itemStyle = this.options[index - 1].itemStyle
-        itemStyle.transform = `scale(${Math.max(ratio, 0.8)})`
-        itemStyle.filter = `blur(${(1 - ratio) * 30}px)`
-      }
-      if (index < this.options.length - 1) {
-        itemStyle = this.options[index + 1].itemStyle
-        itemStyle.transform = `scale(${Math.max(ratio, 0.8)})`
-        itemStyle.filter = `blur(${(1 - ratio) * 30}px)`
-      }
+
+      activeItem.itemStyle.transform = `translate(${activeItem.ratioX}%)`
+      // activeItem.itemStyle.left = `${activeItem.ratioX}%`
+      this.activeId = this.options[this.activeIndex].id
     }
   }
 }
@@ -242,23 +265,23 @@ export default {
 
 <style lang="scss" scoped>
 .sg-slider {
+  position: relative;
   width: 100%;
   height: 100%;
   box-sizing: border-box;
   overflow: hidden;
-  .wrapper {
-    height: 100%;
-    white-space: nowrap;
-  }
-  .card-item-wrapper {
-    display: inline-block;
+
+  .slider-item {
+    position: absolute;
+    left: 0;
+    top: 0;
     width: 100%;
     height: 100%;
+    overflow: auto;
   }
-  .card-item {
-    width: 100%;
-    height: 100%;
-  }
+}
+.slide-anime .slider-item {
+  transition: transform 300ms, left 300ms;
 }
 </style>
 
